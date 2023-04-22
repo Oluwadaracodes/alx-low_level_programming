d72739b37db75190ffaa63675ffd93111eb5321d
Oluwadaracodes <oluwadaraafon@gmail.com>
2023-04-21 17:54:54 -0700

    digit number of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - 'prints single numbers in base ten'
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
+

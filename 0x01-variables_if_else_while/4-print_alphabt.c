eb0d3ea0c11513cef3d2ad81c9dd9d31bbd57108
Oluwadaracodes <oluwadaraafon@gmail.com>
2023-04-21 17:10:58 -0700

    alphabet without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - 'print lowercase alphabets except q and e'
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a = 97;
+
+	while (a <= 122)
+	{
+		if (a == 101 || a == 113)
+		{
+			a++;
+			continue;
+		}
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}

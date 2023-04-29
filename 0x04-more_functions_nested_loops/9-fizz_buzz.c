2b2915fc634fa3192ae47dd023f993ca8b84b344
Oluwadaracodes <oluwadaraafon@gmail.com>
2023-04-28 20:37:00 -0700

    main

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,27 +11,24 @@ int main(void)
 {
 	int a;
 
-	a = 1;
-
-	while (a <= 100)
+	for (a = 1; a <= 100; a++)
 	{
-		if (a % 15 == 0)
+		if (a % 5 && a % 3 == 0)
 		{
 			printf("FizzBuzz ");
-		}
-		else if (a % 5 == 0)
-		{
-			printf("Buzz ");
 		}
 		else if (a % 3 == 0)
 		{
 			printf("Fizz ");
 		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
 		else
 		{
 			printf("%d ", a);
 		}
-		a++;
 	}
 
 	printf("\n");

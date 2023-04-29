a810ef2996b8aebddae749b300b67254d2cb9b1d
Oluwadaracodes <oluwadaraafon@gmail.com>
2023-04-28 20:11:54 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,28 +17,23 @@ int main(void)
 	{
 		if (a % 15 == 0)
 		{
-			printf("FizzBuzz");
-			printf(" ");
+			printf("FizzBuzz ");
 		}
 		else if (a % 5 == 0)
 		{
-			printf("Buzz");
-			printf(" ");
+			printf("Buzz ");
 		}
 		else if (a % 3 == 0)
 		{
-			printf("Fizz");
-			printf(" ");
+			printf("Fizz ");
 		}
 		else
 		{
-			printf("%d", a);
-			
-			if (a != 100)
-				printf(" ");
+			printf("%d ", a);
 		}
 		a++;
 	}
+
 	printf("\n");
 	return (0);
 }

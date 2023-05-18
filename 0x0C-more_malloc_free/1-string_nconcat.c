47ddcc54ccc75934d3a3d221784bddd4469b2803
Oluwadaracodes <oluwadaraafon@gmail.com>
2023-05-18 21:14:00 +0200

    concatenate string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,19 +18,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *outcome;
 	unsigned int len1 = 0, len2 = 0, a, b;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
+	}
+	if (s2 == NULL)
+	{
 		s2 = "";
 	}
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
 	while (s2[len2] != '\0')
-	{
 		len2++;
-	}
 	if (n > len2)
 	{
 		n = len2;
